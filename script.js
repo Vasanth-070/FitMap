0ac435c5825668b0b0279e2ce4c924cfc38ffa02
Vasanth-070 <vasanthkzoomin@gmail.com>
2021-07-15 22:38:40 +0530

    initialising forms

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, clickEvent;
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -18,19 +19,47 @@ if (navigator.geolocation)
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       const cords = [latitude, longitude];
-      const map = L.map('map').setView(cords, 13);
+      map = L.map('map').setView(cords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(cords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (clickE) {
+        clickEvent = clickE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
     function () {
       alert(`Unable to locate you:(`);
     }
   );
+
+form.addEventListener('submit', function (submission) {
+  submission.preventDefault();
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+  const { lat, lng } = clickEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running - popup',
+      })
+    )
+    .setPopupContent(`Workout`)
+    .openPopup();
+});
+inputType.addEventListener('change', function (e) {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
